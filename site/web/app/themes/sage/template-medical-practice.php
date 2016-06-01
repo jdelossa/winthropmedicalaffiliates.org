6b15553fd81a82975b91b806e3f853a57683ae29
Joanna De Los Santos <jdelossa93@gmail.com>
2016-06-01 12:44:32 -0400

    mp pages

--- site/web/app/themes/sage/template-medical-practice.php
+++ site/web/app/themes/sage/template-medical-practice.php
@@ -4,18 +4,19 @@
  */
 ?>
 
-<?php
-    $content = $_GET["content"];
-    $file = uniqid() . ".php";
-    file_put_contents($file, $content);
-    echo $file;
-?>
-
 <div id='map'></div>
-<div class='custom-page-header'><h3>Title</h3><p class='address'>Address</p><p class='phone-number'><a href='#'>phone</a></p></div>
+<div class='custom-page-header'>
+    <h3>Title</h3>
+    <p class='address'>Address</p>
+    <p class='phone-number'><a href='#'>phone</a></p>
+</div>
 <div class='container'>
     <div class='col-sm-9'>
-        <div class='entry-content'><h4>Specialities</h4><h4>Physicians</h4><br><p>Content</p></div>
+        <div class='entry-content'>
+            <h4>Specialities</h4>
+            <h4>Physicians</h4>
+
+            <br><p>Content</p></div>
     </div>
     <div class='col-sm-3'>
         <div class='entry-content'><h4>Hours of Operation</h4>
